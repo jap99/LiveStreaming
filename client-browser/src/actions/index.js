b25bf29f25c579da27edfc3e7719c37bc7ee7d49
javidpoornasir <javidp.sf@gmail.com>
2021-01-19 14:14:21 -0800

    create the rest of our action creators

--- client-browser/src/actions/index.js
+++ client-browser/src/actions/index.js
@@ -1,7 +1,15 @@
 // ACTION CREATORS
 
 import streams from '../apis/streams';
-import { SIGN_IN, SIGN_OUT, CREATE_STREAM } from './types';
+import { 
+  SIGN_IN, 
+  SIGN_OUT, 
+  CREATE_STREAM, 
+  FETCH_STREAMS,
+  FETCH_STREAM,
+  EDIT_STREAM,
+  DELETE_STREAM
+} from './types';
 
 export const signIn = (userID) => {
   return {
@@ -26,3 +34,25 @@ export const createStream = formValues => async dispatch => {
 //     streams.post('/streams', formValues);
 //   };
 // };
+
+
+export const fetchStreams = () => async dispatch => {
+  const res = await streams.get('/streams');
+  dispatch({ type: FETCH_STREAMS, payload: res.data });
+};
+
+export const fetchStream = (id) => async dispatch => {
+  const res = await streams.get(`/streams/${id}`);
+  dispatch({ type: FETCH_STREAM, payload: res.data });
+};
+
+export const editStream = (id, formValues) => async dispatch => {
+  const res = await streams.put(`/streams/${id}`, formValues);
+  dispatch({ type: EDIT_STREAM, payload: res.data });
+};
+
+export const deleteStream = (id) => async dispatch => {
+  await streams.delete(`/streams/${id}`);
+  dispatch({ type: DELETE_STREAM, payload: id });
+};
+
