9c94325b486756c7c70513266e380dd8ee8f87ad
javidpoornasir <javidp.sf@gmail.com>
2021-01-19 15:57:53 -0800

    fix last commit

--- client-browser/src/history.js
+++ client-browser/src/history.js
@@ -1,3 +1,4 @@
-import createHistory from 'history/createBrowserHistory';   // history comes w/ react-router-dom
+import { createBrowserHistory } from 'history';    // history comes w/ react-router-dom
 
-export default createHistory();
+
+export default createBrowserHistory();
