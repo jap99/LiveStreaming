059c7a395039cf643f1c31a13073889c82600f4a
javidpoornasir <javidp.sf@gmail.com>
2021-01-20 04:20:24 -0800

    modal's cancel button dismisses the modal now

--- client-browser/src/components/streams/StreamDelete.js
+++ client-browser/src/components/streams/StreamDelete.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { connect } from 'react-redux';
+import { Link } from 'react-router-dom';
 import Modal from '../Modal';
 import history from '../../history';
 import { fetchStream } from '../../actions';
@@ -11,10 +12,18 @@ class StreamDelete extends React.Component {
   };
 
   renderActions() {
+    const { id } = this.props.match.params;
     return (
       <React.Fragment>
-        <button className="ui button negative">Delete</button>
-        <button className="ui button">Cancel</button>
+        <button
+          onClick={() => this.props.deleteStream(id)}
+          className="ui button negative"
+        >
+          Delete
+        </button>
+        <Link to="/" className="ui button">
+          Cancel
+        </Link>
       </React.Fragment>
     );
   };
