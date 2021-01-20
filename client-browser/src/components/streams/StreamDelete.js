0c1812fdbc8fc96c5a2cab0c3b4c5cf49f18c55a
javidpoornasir <javidp.sf@gmail.com>
2021-01-20 04:24:05 -0800

    stream deletes & user's sent to index/main page after it deletes

--- client-browser/src/components/streams/StreamDelete.js
+++ client-browser/src/components/streams/StreamDelete.js
@@ -3,7 +3,7 @@ import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
 import Modal from '../Modal';
 import history from '../../history';
-import { fetchStream } from '../../actions';
+import { fetchStream, deleteStream } from '../../actions';
 
 class StreamDelete extends React.Component {
   
@@ -53,5 +53,5 @@ const mapStateToProps = (state, ownProps) => {
 
 export default connect(
   mapStateToProps,
-  { fetchStream }
+  { fetchStream, deleteStream }
 )(StreamDelete);
