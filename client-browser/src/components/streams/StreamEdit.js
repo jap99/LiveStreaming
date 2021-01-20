17aea409be462db9802daa2a9f9f5f29276fa666
javidpoornasir <javidp.sf@gmail.com>
2021-01-19 17:06:21 -0800

    start creation of StreamForm

--- client-browser/src/components/streams/StreamEdit.js
+++ client-browser/src/components/streams/StreamEdit.js
@@ -10,9 +10,12 @@ class StreamEdit extends React.Component {
 
   render() {
     console.log(this.props);
+    if (!this.props.stream) {
+      return <div>Loading...</div>
+    }
     return (
       <div> 
-        Stream Edit 
+        { this.props.stream.title }
       </div>
     );
   };
