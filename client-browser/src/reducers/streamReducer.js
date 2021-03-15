af51e6d08ba547aa053f6dfd2f2a7b6d91d01d38
jap99 <javid.p7777@gmail.com>
2021-03-14 18:23:58 -0700

    refactor switch statement

--- client-browser/src/reducers/streamReducer.js
+++ client-browser/src/reducers/streamReducer.js
@@ -9,17 +9,18 @@ import _ from 'lodash';
 
 export default (state = {}, action) => {
     switch (action.type) {
-        case CREATE_STREAM:
+        case CREATE_STREAM, EDIT_STREAM, FETCH_STREAM:
             return { ...state, [action.payload.id]: action.payload };
         case FETCH_STREAMS:
             return { ...state, ..._.mapKeys(action.payload, 'id') };
-        case FETCH_STREAM:
-            return { ...state, [action.payload.id]: action.payload };
         case DELETE_STREAM:
             return _.omit(state, action.payload);
-        case EDIT_STREAM:
-            return { ...state, [action.payload.id]: action.payload };
         default:
             return state;
     }
-}
+};
+
+/* ----- TO DO ----- for edit & delete stream we need to do a check on the server side also to verify 
+    they're still trying to edit/delete their own stream & that a hacker hasn't simply passed in a different
+    userUID or streamID to bypass anything & do his/her hack
+*/
