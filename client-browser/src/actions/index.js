517432a45b3726075505196e0dd60a0717914775
javidpoornasir <javidp.sf@gmail.com>
2021-03-14 18:02:36 -0700

    add comments

--- client-browser/src/actions/index.js
+++ client-browser/src/actions/index.js
@@ -1,4 +1,8 @@
-// ACTION CREATORS
+// ------- ACTION CREATORS ------- //
+// ------- ACTION CREATORS ------- //
+// ------- ACTION CREATORS ------- //
+// ------- ACTION CREATORS ------- //
+// ------- ACTION CREATORS ------- //
 
 import streams from '../apis/streams';
 import history from '../history';
@@ -52,11 +56,17 @@ export const fetchStream = (id) => async dispatch => {
 // form values are the new values to update the stream with
 export const editStream = (id, formValues) => async dispatch => {
   const res = await streams.patch(`/streams/${id}`, formValues);
+  /* ---- TODO ---- the response we get is given to us here after the stream's deleted from the api server
+      we then dispatch it so it can be updated in the redux store to reflect the deleted stream. We need to 
+      verify that the deletion in the api server AND in the redux store were successful; otherwise, make them 
+      both unsuccessful so that it appears as if nothing at all even occurred.
+  */ 
   dispatch({ type: EDIT_STREAM, payload: res.data });
   history.push('/');
 };
 
 export const deleteStream = (id) => async dispatch => {
+  // ---- TODO --- have the api server confirm deletion was completed successfully & react accordingly ---- // 
   await streams.delete(`/streams/${id}`);
   dispatch({ type: DELETE_STREAM, payload: id });
   history.push('/');
